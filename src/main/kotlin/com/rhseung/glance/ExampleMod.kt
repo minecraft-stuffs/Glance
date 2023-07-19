bf878f6ba206d90fdac22c2fb6f5d290e9901ecf
Hyunseung Ryu <56152093+rhseung@users.noreply.github.com>
2023-07-19 21:31:12 +0900

    import success

--- src/main/kotlin/com/rhseung/glance/ExampleMod.kt
+++ src/main/kotlin/com/rhseung/glance/ExampleMod.kt
@@ -1,15 +1,14 @@
 package com.rhseung.glance
 
+import com.rhseung.abstractlib.api.file.path.URI
 import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object ExampleMod : ModInitializer {
-	private val logger = LoggerFactory.getLogger("glance")
+	val modId = URI.getModId()
+    private val logger = LoggerFactory.getLogger(modId)
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+		logger.info("modId = $modId")
 	}
 }
