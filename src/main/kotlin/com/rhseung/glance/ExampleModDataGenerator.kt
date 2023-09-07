06724456152013b4615a8264bb2096f23ba96e6d
Hyunseung Ryu <56152093+rhseung@users.noreply.github.com>
2023-09-07 20:46:12 +0900

    abstractlib 2 to 3

--- src/main/kotlin/com/rhseung/glance/ExampleModDataGenerator.kt
+++ src/main/kotlin/com/rhseung/glance/ExampleModDataGenerator.kt
@@ -2,8 +2,12 @@ package com.rhseung.glance
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 
 object ExampleModDataGenerator : DataGeneratorEntrypoint {
 	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+		val pack = fabricDataGenerator.createPack()
+
+//		pack.addProvider { output: FabricDataOutput -> TestModelProvider(output) }
 	}
 }
